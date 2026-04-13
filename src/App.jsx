1b22736d0c749c97ffc2b9c9a0e43cd89338c34c
Wainaina Erastus <wainainaerastus2@gmail.com>
2026-04-13 18:38:11 +0300

    loader

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setInitialLoading(false);
-    }, 2000);
+    }, 3000);
     return () => clearTimeout(timer);
   }, []);
 
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,17 +1,14 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { HashLoader } from "react-spinners";
 
 const Loader = () => (
-  <div className="fixed inset-0 flex items-center justify-center bg-white z-[9999]">
-    <motion.div
-      animate={{
-        scale: [1, 1.2, 1],
-        opacity: [0.5, 1, 0.5],
-      }}
-      transition={{ repeat: Infinity, duration: 1.5 }}
-      className="w-16 h-16 bg-blue-600 rounded-full"
-    />
+  <div className="fixed inset-0 z-[9999] flex flex-col items-center justify-center bg-brand-navy backdrop-blur-sm">
+    <HashLoader color="#d6671c" size={80} speedMultiplier={1.2} />
+    <br />
+    <p className="loader  text-white font-semibold animate-pulse">
+      Connecting to homeNet...
+    </p>
   </div>
 );
 
-export default Loader ;
+export default Loader;
