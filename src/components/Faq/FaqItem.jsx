7fffe8cc92c39bdf8c80ec7a478430d161e0ce47
Wainaina Erastus <wainainaerastus2@gmail.com>
2026-04-17 16:07:33 +0300

    faqs refactoring

--- src/components/Faq/FaqItem.jsx
+++ src/components/Faq/FaqItem.jsx
@@ -4,22 +4,26 @@ import { IoIosArrowDropupCircle } from "react-icons/io";
 
 const FaqItem = ({ question, answer, isOpen, toggle }) => {
   return (
-    <div className="border-b border-slate-500 dark:border-white/10 last:border-none">
+    <div className="border-b faq-item border-slate-500  last:border-none">
       <button
         onClick={toggle}
         className="w-full py-6 flex justify-between items-center text-left group transition-all"
       >
-        <span className={`text-lg font-bold transition-colors duration-300 ${
-          isOpen ? "text-brand-orange" : "text-white dark:text-slate-200"
-        }`}>
+        <span
+          className={`text-lg font-bold transition-colors duration-300 ${
+            isOpen ? "text-brand-orange" : "text-brand-orange "
+          }`}
+        >
           {question}
         </span>
         <motion.div
-          animate={{ rotate: isOpen ? 45 : 0, color: isOpen ? "#FF6D00" : "#fbfbfc" }}
+          animate={{
+            rotate: isOpen ? 45 : 0,
+            color: isOpen ? "#FF6D00" : "#FF6D00",
+          }}
           className="text-2xl"
         >
-         
-         <IoIosArrowDropupCircle />
+          <IoIosArrowDropupCircle />
         </motion.div>
       </button>
 
@@ -32,7 +36,7 @@ const FaqItem = ({ question, answer, isOpen, toggle }) => {
             transition={{ duration: 0.3, ease: "easeInOut" }}
             className="overflow-hidden"
           >
-            <p className="pb-9 mt-7 text-white dark:text-slate-400 leading-relaxed max-w-2xl">
+            <p className="pb-9 mt-7 text-dark  leading-relaxed max-w-2xl">
               {answer}
             </p>
           </motion.div>
