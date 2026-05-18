a5f4837a25a8fe5988b03d3f96485ed99feca266
Erastus Wainaina <164462952+wainaina-mwangi@users.noreply.github.com>
2026-05-18 17:24:41 +0300

    Remove dark background class from Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ import WhatsAppButton from "../components/WhatsAppButton";
 
 const Home = () => {
   return (
-    <div className="overflow-hidden dark:bg-yellow">
+    <div className="overflow-hidden">
       <Hero />
       <Services />
       <About />
