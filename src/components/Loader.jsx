0979b1ca627896bf362ce3f8a542ae4bf8c6fb59
Erastus Wainaina <164462952+wainaina-mwangi@users.noreply.github.com>
2026-05-21 17:27:15 +0300

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import { HashLoader } from "react-spinners";
 
 const Loader = () => (
   <div className="fixed inset-0 z-[9999] flex flex-col items-center justify-center bg-brand-navy backdrop-blur-sm">
-    <HashLoader color="#d6671c" size={80} speedMultiplier={1.2} />
+    <BarLoader color="#d6671c" size={80} speedMultiplier={1.2} />
 
   </div>
 );
