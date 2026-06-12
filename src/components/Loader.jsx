8d5329d21b3f44d6c9a93924c4e21de08cf854da
Erastus Wainaina <164462952+wainaina-mwangi@users.noreply.github.com>
2026-06-12 11:32:04 +0300

    Change Loader component paragraph margin

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -33,7 +33,7 @@ const Loader = () => {
       </div>
 
       <motion.p
-        className="mt-8 text-2xl font-semibold text-white text-xs tracking-[0.25em] uppercase  text-white text-center px-4"
+        className="mb-10 text-2xl font-semibold text-white text-xs tracking-[0.25em] uppercase  text-white text-center px-4"
         animate={{ opacity: [0.4, 1, 0.4] }}
         transition={{ duration: 2, repeat: Infinity, ease: "easeInOut" }}
       >
